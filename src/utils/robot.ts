06b606f70cf01f3a715b09f66c628637ee4b3804
SabrinaMiao <sabrinamsh@gmail.com>
2024-11-23 19:59:20 +0100

    Create robot.ts

---
+++ src/utils/robot.ts
@@ -0,0 +1,46 @@
+import { Direction } from "../types/direction";
+
+class Robot {
+    x: number;
+    y: number;
+    visited: Set<string>;
+    position: string;
+  
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+      this.visited = new Set();
+      this.position = `{${x}: ${y}}`;
+      this.visited.add(this.position);
+    }
+  
+    move(direction: string, steps: number) {
+      for (let i = 0; i < steps; i++) {
+        switch (direction) {
+            case Direction.NORTH:
+                this.y += 1;
+                break;
+            case Direction.SOUTH:
+                this.y -= 1;
+                break;
+            case Direction.EAST:
+                this.x += 1;
+                break;
+            case Direction.WEST:
+                this.x -= 1;
+                break;
+            default:
+              throw new Error('Invalid direction');
+                break;
+        }
+        this.position = `{${this.x}: ${this.y}}`;
+        this.visited.add(this.position);
+      }
+    }
+
+    getCleanedPlaces() {
+        return this.visited.size;
+      }
+    }
+  
+  export default Robot;
